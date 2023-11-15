44537ffa69965f0e04dc598534378256a98681b7
Alokray007 <alokr6927@gmail.com>
2023-11-16 00:06:57 +0530

    Environment Setup Done

---
+++ app/(root)/page.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@/components/ui/button"
+
+
+export default function Home() {
+    return (
+      <>
+        <h1>Hello Admin Dashboard</h1>
+        <div>
+          <Button className="m-8" >Click Me</Button>
+        </div>
+
+
+      </>
+
+    )
+  }
